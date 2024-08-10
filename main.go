51afd3024034013be624d00d72d29342e98a92a5
Harry-Kachigamba <harrykachigamba22@outlook.com>
2024-08-11 00:50:36 +0200

    added POST and PATCH methods

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 //importing http_protocol and gin web framework package
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -32,9 +33,93 @@ func getNovels(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, novels)
 }
 
+// method to get novel by id
+func novelById(c *gin.Context) {
+	id := c.Param("id")
+	novel, err := getNovelById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Novel not found."})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, novel)
+}
+
+// method to checkout a book
+func checkoutNovel(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+	}
+
+	novel, err := getNovelById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Novel not found."})
+		return
+	}
+
+	if novel.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Novel not available."})
+		return
+	}
+
+	novel.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, novel)
+}
+
+// method to return novel
+func returnNovel(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	novel, err := getNovelById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Novel not found."})
+		return
+	}
+
+	novel.Quantity += 1
+	c.IndentedJSON(http.StatusOK, novel)
+}
+
+// method to loop through a collection and check
+func getNovelById(id string) (*novel, error) {
+	for i, n := range novels {
+		if n.ID == id {
+			return &novels[i], nil
+		}
+	}
+
+	return nil, errors.New("novel not found")
+}
+
+// method to add novel
+func createNovel(c *gin.Context) {
+	var newNovel novel
+
+	if err := c.BindJSON(&newNovel); err != nil {
+		return
+	}
+
+	novels = append(novels, newNovel)
+	c.IndentedJSON(http.StatusCreated, newNovel)
+}
+
 // main method
 func main() {
 	router := gin.Default()
 	router.GET("/novels", getNovels)
+	router.POST("/novels", createNovel)
+	router.GET("/novels/:id", novelById)
+	router.PATCH("/checkout", checkoutNovel)
+	router.PATCH("/return", returnNovel)
 	router.Run("localhost:8080")
 }
